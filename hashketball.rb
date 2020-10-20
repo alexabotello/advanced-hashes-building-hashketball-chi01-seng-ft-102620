504653523ec11cb9c811427f3f96452014b156ea
Alexa Botello <abotello20@yahoo.com>
2020-10-20 01:29:53 +0000

    Automatically backed up by Learn

--- hashketball.rb
+++ hashketball.rb
@@ -1,13 +1,3 @@
-def game_hash = {
-  :home =>{
-    :team_name => {"Brooklyn Nets"}
-    :colors => {"black", "white"}
-    :players =>{}
-  },
-  :away =>{
-    :team_name => {}
-    :colors => {}
-    :players => {}
-  }
+def game_hash {
 }
 end 
