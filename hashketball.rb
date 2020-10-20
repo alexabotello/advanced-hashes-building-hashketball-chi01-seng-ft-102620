c93de56deb1e7d0e14ad6b60807ee37b52547c78
Alexa Botello <abotello20@yahoo.com>
2020-10-20 01:45:05 +0000

    Automatically backed up by Learn

--- hashketball.rb
+++ hashketball.rb
@@ -1,4 +1,14 @@
-def game_hash {
-  
+def game_hash
+  hashketball = {
+  :home => {
+    :team_name => { "Brooklyn Nets" }
+    :colors => { "Black", "White" }
+    :players => [{player_name: "Alan Anderson", number: "0", shoe: "16", points: "22", rebounds: "12", assists: "12", steals: "3", blocks: "1", slam_dunks: "1"}, ]
+  }
+  :away => {
+    :team_name => { "Charlotte Hornets" }
+    :colors => { "Turquoise", "Purple" }
+    :players => [{}]
+  }
 }
 end 
